ab377947223aba33790e24cfd95cdedd4afd2b00
Jasper0077 <jasperhwong@hotmail.com>
2023-04-23 23:16:44 +0800

    update navbar with mobile menu view

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,6 +9,7 @@ import cn from "classnames";
 
 const Navbar = () => {
     const [active, setActive] = React.useState<string>("");
+    const [toggle, setToggle] = React.useState<boolean>(false);
     return (
         <nav
             className={cn(
@@ -31,6 +32,64 @@ const Navbar = () => {
                         <span className="sm:block hidden">| 3D Portfolio</span>
                     </p>
                 </Link>
+                <ul className="list-none hidden sm:flex flex-row gap-10">
+                    {navLinks.map((link) => {
+                        return (
+                            <li
+                                key={link.id}
+                                className={`${
+                                    active === link.id ?
+                                    "text-white" :
+                                    "text-secondary"
+                                } hover:text-white text-[18px] font-medium cursor-pointer`}
+                                onClick={() =>  setActive(link.id)}
+                            >
+                                <a href={`#${link.id}`}>{link.title}</a>
+                            </li>
+                        )
+                    })}
+                </ul>
+                <div className="sm:hidden flex flex-1 justify-end items-center">
+                    <img 
+                        src={toggle ? close : menu}
+                        alt="menu"
+                        className={cn(
+                            "w-[28px] h-[28px]",
+                            "object-contain cursor-pointer"
+                        )}
+                        onClick={() => setToggle(!toggle)}
+                    />
+
+                    <div className={cn(
+                        !toggle ? "hidden" : "flex",
+                        "p-6 bg-gray-200 absolute rounded-md",
+                        "top-20 right-0 mx-4 my-2 min-w-[140px]"
+                    )}>
+                        <ul 
+                            className={cn(
+                                "list-none", 
+                                "flex flex-col justify-start items-start gap-4"
+                            )}>
+                            {navLinks.map((link) => {
+                                return (
+                                    <li
+                                        key={link.id}
+                                        className={cn(
+                                            active === link.id ? "text-primary hover:text-primary" : "text-secondary hover:text-primary",
+                                        "text-[18px] font-medium cursor-pointer"
+                                        )}
+                                        onClick={() =>  {
+                                            setActive(link.id);
+                                            setToggle(!toggle);
+                                        }}
+                                    >
+                                        <a href={`#${link.id}`}>{link.title}</a>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </div>
+                </div>
             </div>
         </nav>
     )
