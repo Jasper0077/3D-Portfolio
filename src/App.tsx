30078b2bc50d0f3e413d1210459ab44426e7c7a0
Jasper0077 <jasperhwong@hotmail.com>
2023-04-23 18:05:10 +0800

    import components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import "./App.css";
+import  { BrowserRouter } from "react-router-dom";
+import { Navbar, Hero, About, Experience, Tech, Works, Contact, Feedbacks, StarsCanvas } from "./components";
 
 function App() {
   return (
-    <div>
-      <h1>3D Developer Portfolio</h1>
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
