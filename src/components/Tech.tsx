7083cd489a2e57a0bae1bf18eaf030dbf0d3d8e7
Jasper0077 <jasperhwong@hotmail.com>
2023-05-01 22:35:27 +0800

    init tech section

--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -1,9 +1,18 @@
+import React from "react";
+import SectionWrapper from "./Wrapper";
+import { BallCanvas } from "./canvas/Ball";
+import { technologies } from "../constants";
+
 const Tech = () => {
     return (
-        <div>
-            Tech
+        <div className="flex flex-row flex-wrap justify-center gap-10">
+            {technologies.map((technology, index) => {
+                return (
+                    <BallCanvas key={index} icon={technology.icon} />
+                )
+            })}
         </div>
     )
 }
 
-export default Tech;
+export default SectionWrapper("technologies", Tech);
