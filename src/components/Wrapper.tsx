7ac5c73259daa879fe8566a3e254512341863f03
Jasper0077 <jasperhwong@hotmail.com>
2023-05-04 22:34:58 +0800

    fix wrapper type

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -2,22 +2,20 @@ import { motion } from "framer-motion";
 import { staggerContainer } from "../utils/motion";
 import { styles } from "../styles";
 
-const SectionWrapper = (id: string, Component: () => JSX.Element) => {
+const SectionWrapper = <P extends object>(id: string, Component: React.ComponentType<P>): React.FC<P> => (props) => {
     if (!Component) return null;
-    return function HOC() {
-        return (
-            <motion.section
-                variants={staggerContainer()}
-                initial="hidden"
-                whileInView="show"
-                viewport={{ once: true, amount: 0.25 }}
-                className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
-            >
-                <span className="hash-span" id={id}>&nbsp;</span>
-                <Component />
-            </motion.section>
-        )
-    }
+    return (
+        <motion.section
+            variants={staggerContainer()}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: true, amount: 0.25 }}
+            className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+        >
+            <span className="hash-span" id={id}>&nbsp;</span>
+            <Component {...props as P}/>
+        </motion.section>
+    )
 }
 
 export default SectionWrapper;
